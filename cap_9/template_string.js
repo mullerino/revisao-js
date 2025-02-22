820bc310635690151b1f7fac2910761dc05e4ac1
Leandro <leandromllr9@gmail.com>
2025-02-22 11:09:31 -0300

    letiura do capitulo 9

---
+++ cap_9/template_string.js
@@ -0,0 +1,9 @@
+/*
+
+São estruturas que nos permite formar expressões com strings
+usando funcionalidades como interpolação e multilinhas.
+
+*/
+
+const nome = 'Muller'
+console.log(`Olá ${nome}`)
